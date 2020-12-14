1b91675b6a3ef43ea27f0447dc081430c57b9612
yoshiya-03 <yoshiya03y07@gmail.com>
2020-12-14 05:15:36 +0000

    [Update]ユーザー周りの機能の追加

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,38 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:titel, :body)
+  end
+
+end
+
+
