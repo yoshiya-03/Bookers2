85d0a755a1d4a6a84afd658659f47db32ccba23a
yoshiya-03 <yoshiya03y07@gmail.com>
2021-01-09 07:25:09 +0000

    [Add]

--- Gemfile
+++ Gemfile
@@ -64,4 +64,5 @@ gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
 gem 'bootstrap', '~> 4.5'
 gem 'jquery-rails'
+gem 'jp_prefecture'
 gem 'font-awesome-sass', '~> 5.13'
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,5 +25,16 @@ class User < ApplicationRecord
   def email_changed?
    false
   end
+  
+  jp_prefecture :prefecture_code
+
+ def prefecture_name
+  JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+ end
+
+ def prefecture_name=(prefecture_name)
+  self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+ end
+
 
 end
