9c4afba636c50e622d4e480442c1c5820d1e14f0
yoshiya-03 <yoshiya03y07@gmail.com>
2021-01-28 01:33:45 +0000

    [add]

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,15 +26,16 @@ class User < ApplicationRecord
    false
   end
   
+  include JpPrefecture
   jp_prefecture :prefecture_code
-
- def prefecture_name
-  JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
- end
-
- def prefecture_name=(prefecture_name)
-  self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
- end
+  
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+  
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
 
 
 end
